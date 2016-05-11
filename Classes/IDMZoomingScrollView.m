6e242b5f4fc38fda54d9ce008f3f7a9e1084c565
Ruijun Li <noark9@gmail.com>
2016-05-12 00:44:55 +0800

    tap to close

--- Classes/IDMZoomingScrollView.m
+++ Classes/IDMZoomingScrollView.m
@@ -261,7 +261,7 @@
 #pragma mark - Tap Detection
 
 - (void)handleSingleTap:(CGPoint)touchPoint {
-	[_photoBrowser performSelector:@selector(toggleControls) withObject:nil afterDelay:0.2];
+	[_photoBrowser performSelector:@selector(doneButtonPressed:) withObject:nil afterDelay:0.2];
 }
 
 - (void)handleDoubleTap:(CGPoint)touchPoint {
